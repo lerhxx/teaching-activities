0bb83e9e8d6da922bb7f80cbbf868fcc695a58df
lerhxx <407785526@qq.com>
2017-02-06 15:17:39 +0800

    initialized data

--- design/server/app.js
+++ design/server/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const db = require('./db');
 const fs = require('fs');
 const path = require('path');
+const resolve = file => path.resolve(__dirname, file);
 const app = module.exports = express();
 
 app.set('port', (process.env.port || 3000));
--- design/server/db.js
+++ design/server/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const db = mongoose.connection;
+const list = require('../json/list.json');
+const footerLink = require('../json/footer-link.json');
+const search = require('../json/search.json');
 
 mongoose.connect('mongodb://localhost/design');
 
@@ -21,10 +24,68 @@ const articleSchema = new Schema({
 	content: String
 })
 
+const listSchema = new Schema({
+	url: String,
+	title: String,
+	abs: String
+})
+
+const footLinkSchema = new Schema({
+	url: String,
+	text: String
+})
+
+const searchSchema = new Schema({
+	faculties: [String],
+	types: [String],
+	timeliness: [String]
+})
+
 const Models = {
 	User: mongoose.model('User', UserSchema),
-	Artical: mongoose.model('Artical', articleSchema)
+	Artical: mongoose.model('Artical', articleSchema),
+	List: mongoose.model('List', listSchema),
+	FootLink: mongoose.model('FootLink', footLinkSchema),
+	Search: mongoose.model('Search', searchSchema),
+	initialized: false
 }
+
+const initialize = () => {
+	Models.List.find(null, (err, doc) => {
+		if(err) {
+			console.error(err);
+		}else if(!doc.length) {
+			console.log('Database opens for the first time...')
+			//TODO
+			list.lists.map(item => new Models['List'](item).save());
+			footerLink.map(item => new Models['FootLink'](item).save());
+			new Models['Search'](search).save();
+			Models.List.find(null, (err, doc) => {
+				if(err) {
+					return console.error(err)
+				}
+				console.log(doc)
+			})
+		}else {
+			Models.initialized = true;
+		}
+	})
+}
+
+const listP = new Promise((resolve, rej) => {
+	list.lists.map(item => new Models['List'](item).save());
+	console.log('list initialized')
+})
+
+const footLinkP = new Promise((resolve, rej) => {
+	footerLink.map(item => new Models['FootLink'](item).save());
+	console.log('footerLink initialized')
+})
+
+const searchP = new Promise((resolve, rej) => {
+	new Models['Search'](search).save();
+	console.log('searchP initialized')
+})
 
 db.on('error', () => {
 	console.error('Database connection error.');
@@ -32,6 +93,7 @@ db.on('error', () => {
 
 db.on('open', () => {
 	console.log('The database has connected.')
+	initialize();
 })
 
 module.exports = Models;
