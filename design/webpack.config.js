0bd8aecf8450bc81093361d0c555fe9a476eb541
lerhxx <407785526@qq.com>
2017-02-20 09:43:24 +0800

    merge

--- design/webpack.config.js
+++ design/webpack.config.js
@@ -1,10 +1,13 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-	entry: './src/main.js',
+	entry: {
+		app: ['./src/main.js']
+	},
 	output: {
-		path: './dist',
-		publicPath: './dist/',
+		path: path.resolve(__dirname, './dist/js'),
+		publicPath: '/dist/',
 		filename: 'build.js'
 	},
 	module: {
@@ -12,17 +15,17 @@ module.exports = {
 			test: /\.vue$/,
 			loader: 'vue'
 		}, {
-			test: /\.scss$/,
+			test: /\.styl$/,
 			exclude: /node_modules/,
-			loaders: ["style", "css", "sass"]
+			loaders: ["style", "css", "stylus"]
 		}, {
 			test: /\.js$/,
 			loader: 'babel?presets=es2015',
 			exclude: /node_modules/
 		}, {
-			test: /\.(png|jpg|gif)$/,
+			test: /\.(png|jpg|gif|svg)$/,
 			exclude: /node_modules/,
-			loader: 'url?name=[name].[ext]?[hash]'
+			loader: 'url?limit=8192'
 		}]
 	},
 	babel: {
@@ -32,6 +35,14 @@ module.exports = {
 		extensions: ['', '.js', '.vue'],
 		alias: {
 			vue: 'vue/dist/vue.js'
+		}
+	},
+	devServer: {
+		inline: true,
+		hot: true,
+		'/get': {
+			target: 'localhost:3000',
+			secure: false
 		}
 	}
 }
